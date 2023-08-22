91f10f3c142c63b279edacc5d4119274ae22bc65
Ronald Paek <ronaldpaek@gmail.com>
2023-08-22 04:07:29 -0700

    Move all images to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <Box className="blue-box" />
       </Section>
       <Section title="3">
-        <img src="assets/meme.webp" alt="meme" />
+        <img src="/meme.webp" alt="meme" />
       </Section>
       <Section title="4">
         <div className="box-container">
